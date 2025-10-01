69315e3ad4d8ca5124ff488503b02f96506ee819
Sniger <di.snegur@gmail.com>
2025-10-01 08:40:06 +0200

    fix: add elements to dynamic token info section

--- src/components/premarket/PremarketBaseInfo.tsx
+++ src/components/premarket/PremarketBaseInfo.tsx
@@ -5,6 +5,7 @@ import { getTimeLeftLabel } from "@utils/premarket";
 import { View, Image } from "react-native";
 import { Button, Text, useTheme } from "react-native-paper";
 import { ExpandableText } from '@components/base/ExpandableText';
+import Svg, { Circle, Path } from 'react-native-svg';
 
 interface PremarketBaseInfoProps {
   tokenMainInfo: TokenMainInfo;
@@ -15,6 +16,11 @@ export function PremarketBaseInfo({ tokenMainInfo, isMobile}: PremarketBaseInfoP
   const theme = useTheme();
   const { left } = useIsMobileForTwoScreenWithDemention();
 
+  // Function to truncate address/identifier
+  const truncateAddress = (address: string) => {
+    if (address.length <= 10) return address;
+    return `${address.slice(0, 4)}...${address.slice(-4)}`;
+  };
 
   const button = (state: "premarket" | "canceled" | "finished") => {
     return state === 'premarket' ? 
@@ -22,18 +28,27 @@ export function PremarketBaseInfo({ tokenMainInfo, isMobile}: PremarketBaseInfoP
       textColor={theme.colors.secondary}
       buttonColor={theme.colors.onSecondary}
       focusable={false}
+      style={{ borderRadius: 10 }}
+      contentStyle={{marginLeft:8, marginRight:8, maxHeight:30}}
+      labelStyle={{fontSize:15, fontWeight: 'bold'}}
     >Premarket</Button>
     ) : state === 'finished' ? (
     <Button
-      textColor={theme.colors.onPrimary}
-      buttonColor={theme.colors.primary}
+      textColor={theme.colors.primary}
+      buttonColor={theme.colors.onPrimary}
       focusable={false}
+      style={{ borderRadius: 10 }}
+      contentStyle={{marginLeft:8, marginRight:8, maxHeight:30}}
+      labelStyle={{fontSize:15, fontWeight: 'bold'}}
     >Launched</Button>
   ) : state === 'canceled' ? (
     <Button
-      textColor={theme.colors.onError}
-      buttonColor={theme.colors.error}
+      textColor={theme.colors.error}
+      buttonColor={theme.colors.onError}
       focusable={false}
+      style={{ borderRadius: 10 }}
+      contentStyle={{marginLeft:8, marginRight:8, maxHeight:30}}
+      labelStyle={{fontSize:15, fontWeight: 'bold'}}
     >Refunded</Button>
   ) : (
     <Button
@@ -114,13 +129,58 @@ export function PremarketBaseInfo({ tokenMainInfo, isMobile}: PremarketBaseInfoP
           justifyContent: "flex-start",
           alignItems: "center",
           flexDirection: "row",
-          gap: 16,
+          gap: 10,
         }}
       >
         {button(tokenMainInfo.state)}
-        <Text variant="labelLarge" style={{ color: theme.colors.secondary }}>
+        {tokenMainInfo.state === 'finished' && (
+          <Text variant="bodySmall" style={{ color: theme.colors.onSurfaceVariant, fontSize: 15 }}>
+            {/* CHANGE tokenMainInfo.premarketPubkey to tokenMainInfo.mintAddress later!!! */}
+            {tokenMainInfo?.premarketPubkey ? truncateAddress(tokenMainInfo.premarketPubkey.toString()) : "No token address available!"} 
+          </Text>
+        )}
+        {tokenMainInfo.state === 'finished' && (
+          <Svg width={14} height={14} viewBox="0 0 12 12">
+            <Path
+              d="M10.9513 2.85078C10.9513 1.75312 10.0615 0.863281 8.96384 0.863281H3.76884C3.4996 0.863281 3.28134 1.08154 3.28134 1.35078C3.28134 1.62002 3.4996 1.83828 3.76884 1.83828H8.96384C9.52303 1.83828 9.97634 2.29159 9.97634 2.85078V8.6037C9.97634 8.87294 10.1946 9.0912 10.4638 9.0912C10.7331 9.0912 10.9513 8.87294 10.9513 8.6037V2.85078Z"
+              fill={theme.colors.onSurfaceVariant}
+            />
+            <Path
+              d="M8.71858 4.33231C8.71858 3.64886 8.16453 3.09481 7.48108 3.09481H2.28608C1.60263 3.09481 1.04858 3.64886 1.04858 4.33231V9.89992C1.04858 10.5834 1.60263 11.1374 2.28608 11.1374H7.48108C8.16453 11.1374 8.71858 10.5834 8.71858 9.89992V4.33231ZM7.48108 4.06981C7.62606 4.06981 7.74358 4.18734 7.74358 4.33231V9.89992C7.74358 10.0449 7.62606 10.1624 7.48108 10.1624H2.28608C2.14111 10.1624 2.02358 10.0449 2.02358 9.89992V4.33231C2.02358 4.18734 2.14111 4.06981 2.28608 4.06981H7.48108Z"
+              fill={theme.colors.onSurfaceVariant}
+            />
+          </Svg>
+        )}
+        <Text variant="bodySmall" style={{ color: theme.colors.secondary, fontWeight: '100', fontSize: 13 }}>
           {deadlineText}
         </Text>
+        {(tokenMainInfo.state === 'premarket' || tokenMainInfo.state === 'canceled') && (
+          <Svg width={20} height={20} viewBox="0 0 24 24">
+            <Circle
+              cx="12"
+              cy="12"
+              r="9"
+              stroke="#666666"
+              strokeWidth="1.5"
+              fill="transparent"
+            />
+            <Path
+              d="M12,13c0-2,3-1,3-4S9,6,9,9"
+              stroke="#666666"
+              strokeWidth="2"
+              fill="transparent"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <Path
+              d="M12.05,17 L11.95,17"
+              stroke="#666666"
+              strokeWidth="2"
+              fill="transparent"
+              strokeLinecap="round"
+            />
+          </Svg>
+        )}
       </View>
     </View>
   );
