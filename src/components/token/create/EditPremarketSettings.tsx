8711930adbe3f26e568e9e6d1e36a1e016c00840
blockorol <blockchainkorol@gmail.com>
2025-11-27 08:35:17 +0100

    RVL-461: fix comments and remove donuts

--- src/components/token/create/EditPremarketSettings.tsx
+++ src/components/token/create/EditPremarketSettings.tsx
@@ -11,10 +11,8 @@ import BN from "bn.js";
 import { useIsMobileWithDemention } from "@hooks/useIsMobile";
 import { ExtendedMD3Colors } from "@theme/types";
 import { convertSolToPercentOnStart } from "@services/pumpfun/adds";
-import { DonutWithLegend } from "@components/base/DonutWithLegend";
 import { round } from "@utils/numbers";
-import { makeTransparent } from "@utils/colors";
-import { convertLamportToSmallCount, convertSmallCountToLamport } from "@utils/premarket";
+import { convertSmallCountToLamport } from "@utils/premarket";
 import { TOKEN_CONVERTOR_SETTINGS } from "env";
 
 const DEFAULT_PREMARKET_GOAL_SOL = 5;
@@ -43,18 +41,9 @@ export default function EditPremarketSettingsForm({
   const colors = theme.colors as ExtendedMD3Colors;
   const minPremarketSol = tokenomicsData?.creatorInitialBuy??DEFAULT_PREMARKET_GOAL_SOL
   
-  const initialBuyPersent = 
-    tokenomicsData?.creatorInitialBuy ? 
-      round(convertSolToPercentOnStart(tokenomicsData?.creatorInitialBuy), 1)
-      : undefined
-
-  const [percent, setPercent] = useState(0);
   const [premarketGoalSol, setPremarketGoalSol] = useState(
     presetData?.goal_sol ??minPremarketSol
     );
-
-  const [premarketGoalLamp, setPremarketGoalLamp] = useState<BN>
-      (convertSmallCountToLamport(presetData?.goal_sol??minPremarketSol));
   const [goalError, setGoalError] = useState<string | null>(null);
 
 
@@ -76,8 +65,6 @@ export default function EditPremarketSettingsForm({
 
   const changeSliderPremarketValue = (value: number) => {
     setPremarketGoalSol(value)
-    setPremarketGoalLamp(convertSmallCountToLamport(value))
-    setPercent(convertSolToPercentOnStart(value))
     if (value < minPremarketSol) {
       setGoalError("The premarket goal must exceed the initial buy amount")
     } else {
@@ -189,40 +176,6 @@ export default function EditPremarketSettingsForm({
                 {goalError??" "}
               </HelperText>
             </View>
-            <View style={{ marginTop: 24 }}>
-              <DonutWithLegend
-                slices={[
-                  {
-                    value: round(percent, 1),
-                    additional: premarketGoalSol.toFixed(2),
-                    label: "Premarket",
-                    color: theme.colors.primary,
-
-                    subSlices: initialBuyPersent?{
-                      restColor: makeTransparent(theme.colors.onPrimary, 0.7),
-                      slices: [{
-                        value: initialBuyPersent??0,
-                        label: "Creator (you) buy",
-                        color:  makeTransparent(theme.colors.onPrimary, 0.5),
-                        additional: tokenomicsData?.creatorInitialBuy.toFixed(2)
-                      }]
-                    }:undefined
-                  },
-                  {
-                    value: round(80 - percent, 1),
-                    label: "Bonding curve",
-                    color: theme.colors.onSurface,
-                  },
-                  {
-                    value: 20,
-                    label: "Pumpswap pool",
-                    color: theme.colors.secondary,
-                  },
-                ]}
-              />
-            </View>
-            
-            
           </View>
         </View>
         <View style={{ marginTop: 16, paddingBottom: isMobile ? 8 : 16 }}>
